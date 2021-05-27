4f48dba8eff9de9ac84d037dd6cc4298113744e3
Tester798 <Tester798@users.noreply.github.com>
2021-05-27 22:51:33 +0300

    6.58
    * Fixed: Message highlight when clicked on username in resub notification

--- ffzenhancing.js
+++ ffzenhancing.js
@@ -1,6 +1,6 @@
 'use strict';
 (() => {
-    let version = '6.57';
+    let version = '6.58';
     let notify_icon = __ffzenhancing_base_url + 'notify.ico';
     let notify_icon_original = document.querySelector('link[rel="icon"]') && document.querySelector('link[rel="icon"]').href;
     let ffzenhancing_focus_input_area_after_emote_select;
@@ -341,6 +341,7 @@
         if (
             el.classList.contains('chat-author__display-name') ||
             el.classList.contains('chat-line__message-mention') && !el.classList.contains('ffz-i-threads') ||
+            el.parentNode.matches('span.chatter-name[role="button"]') ||
             (el.classList.contains('tw-link') && el.parentNode.parentNode.parentNode.parentNode.classList.contains('viewer-card-header__display-name')) // chatter name link in viewer card
         ) {
             return true;
