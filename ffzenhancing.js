53cdf8fc6952f556e696231c0ff77be1e825fd35
Tester798 <Tester798@users.noreply.github.com>
2022-03-31 18:45:51 +0300

    6.72
    * Fixed: "Double-Click Username to Reply" not working in new user list layout

--- ffzenhancing.js
+++ ffzenhancing.js
@@ -1,6 +1,6 @@
 'use strict';
 (() => {
-    let version = '6.71';
+    let version = '6.72';
     let notify_icon = __ffzenhancing_base_url + 'notify.ico';
     let notify_icon_original = document.querySelector('link[rel="icon"]') && document.querySelector('link[rel="icon"]').href;
     let ffzenhancing_focus_input_area_after_emote_select;
@@ -390,7 +390,10 @@
             el.classList.contains('chat-line__message-mention') && !el.classList.contains('ffz-i-threads') ||
             el.parentNode.matches('span.chatter-name[role="button"]') ||
             el.parentNode.matches('span.ffz--giftee-name[role="button"]') ||
-            (el.classList.contains('tw-link') && el.parentNode.parentNode.parentNode.parentNode.classList.contains('viewer-card-header__display-name')) // chatter name link in viewer card
+            (el.classList.contains('tw-link') && (
+                el.parentNode.parentNode.classList.contains('chatter-list-item') || // chatter name in chatter list
+                el.parentNode.parentNode.parentNode.parentNode.classList.contains('viewer-card-header__display-name') // chatter name link in viewer card
+            ))
         ) {
             return true;
         }
